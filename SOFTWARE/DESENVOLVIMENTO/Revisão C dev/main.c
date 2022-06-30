1d9bbe52c238b4f1e319db3458cc5fb843661f2e
Stenio Rodrigues <30318192+oinetsrv@users.noreply.github.com>
2022-06-30 16:51:31 -0300

    revisao em c

---
+++ SOFTWARE/DESENVOLVIMENTO/Revisão C dev/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> // biblioteca de matemática
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int soma=0,
+	aoba=1;
+void main(){
+	 // %d  inteiro
+	 //%f   float
+	 //%c	caracter
+	 //%s	string
+	 //%%	 coloca % na tela 
+	 //sleep(1)  == delay(1000)
+	 //& ponteiro endereço da memória
+	 // declarar ponteiro
+	 //int *ptr 
+	 /*
+	 ptr = &aoba // o ponteiro recebeu o endereço de memoria da variavel aoba
+	 
+	 */
+	printf("STM32 Curso: %d e o aoba...%d",soma,aoba);
+}// end man
